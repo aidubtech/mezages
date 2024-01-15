ca6329388c9aa77396935a54cd910095c552f51c
Abdulfatai Aka <abdulfataiaka@gmail.com>
2024-01-15 04:30:39 +0100

    implement capitalization

--- src/mezages.py
+++ src/mezages.py
@@ -1,17 +1,13 @@
 import re
-import copy
-from typing import cast, Any
+from typing import cast, Any, TypedDict, Optional
 
 
 base_path = 'base'
-
 entity_placeholder = '{entity}'
 
-
 MezagesStore = dict[str, list[str]]
-
 MezagesInputStore = dict[str, (set[str] | list[str] | tuple[str])]
-
+MezagesPathConfig = TypedDict('MezagesPathConfig', { 'substitute': Optional[str] })
 
 class MezagesError(Exception):
     pass
@@ -24,36 +20,44 @@ class Mezages:
     def __init__(self, store: Any = dict()) -> None:
         self.__store = self.__ensure_store(store)
 
-    @property
-    def store(self) -> MezagesStore:
-        return copy.deepcopy(self.__store)
+    def flat(self, only: list[str] = list(), omit: list[str] = list()) -> list[str]:
+        '''
+        Produce a flattened array of resolved messages from all or selected paths
+        '''
+        return [message for bucket in self.slice(only=only, omit=omit).values() for message in bucket]
 
-    def export(self, only: list[str] = list(), omit: list[str] = list()) -> MezagesStore:
+    def slice(self, only: list[str] = list(), omit: list[str] = list(), resolve: bool = True) -> MezagesStore:
+        '''
+        Produce a mapping of path to resolved or unresolved messages from all or selected paths
+        '''
         result: MezagesStore = dict()
 
         for path, bucket in self.__store.items():
             if (not only or path in only) and (not omit or path not in omit):
-                result[path] = self.__resolve_messages(path, bucket)
+                result[path] = self.__resolve_messages(path, bucket) if resolve else list(bucket)
 
         return result
 
     def __is_path(self, key: Any) -> bool:
         return isinstance(key, str) and bool(re.fullmatch(self.PATH_REGEX, key))
 
-    def __get_entity_placeholder_value(self, path: str) -> str:
-        # Use value from configuration before defaulting to the path string
-        return ('' if path == base_path else path).strip()
-
     def __resolve_messages(self, path: str, messages: list[str]) -> list[str]:
-        entity_placeholder_value = self.__get_entity_placeholder_value(path)
+        path_config: MezagesPathConfig = {'substitute': None}
+        config_substitute = path_config.get('substitute', None)
+
+        entity_substitute = (
+            (str() if path == base_path else path)
+            if config_substitute is None else config_substitute
+        )
 
         resolved_messages: list[str] = list()
 
         for message in messages:
             if message.startswith(entity_placeholder):
-                # How do we handle capitalizing the first letter and handle edge cases too?
-                # We can set placeholder value for a path to an empty string to behave like base
-                message = message.replace(entity_placeholder, entity_placeholder_value, 1).strip()
+                message = message.replace(entity_placeholder, entity_substitute, 1).strip()
+                # An edge case is when the user does not want us to touch the message
+                if entity_substitute == str(): message = f'{message[0].upper()}{message[1:]}'
+
             resolved_messages.append(message)
 
         return resolved_messages
@@ -69,8 +73,8 @@ class Mezages:
             has_valid_path = self.__is_path(path)
 
             has_valid_bucket = (
-                type(bucket) in (set, list, tuple) and bucket
-                and not any(not isinstance(message, str) for message in bucket)
+                type(bucket) in (set, list, tuple) and bucket and not
+                any(not isinstance(message, str) for message in bucket)
             )
 
             if has_valid_path and has_valid_bucket:
@@ -82,7 +86,7 @@ class Mezages:
             elif has_valid_path and not has_valid_bucket:
                 failures.add(f'Path {repr(path)} has an invalid bucket of messages')
             elif not has_valid_path and not has_valid_bucket:
-                failures.add(f'{repr(path)} is an invalid path with an invalid bucket of messages')                
+                failures.add(f'{repr(path)} is an invalid path with an invalid bucket of messages')
 
         if not failures: return new_store
 
