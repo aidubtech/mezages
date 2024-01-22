723090dec48404b6b2610033ba33371b49d55086
Abdulfatai Aka <abdulfataiaka@gmail.com>
2024-01-22 14:03:58 +0100

    refine and document package

---
+++ tests/test_bucket.py
@@ -0,0 +1,9 @@
+from tests.base_case import BaseCase
+
+
+class TestInit(BaseCase):
+    '''when creating new instances of the path class'''
+
+
+class TestBehaviours(BaseCase):
+    '''when calling public properties and methods on path objects'''
