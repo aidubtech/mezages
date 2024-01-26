7cbe8697198e9b611a944a9b98a8e99d848e7ade
belloibrahv <belloibrahv@gmail.com>
2024-01-26 04:28:48 +0100

    Implement union method in Sack class

--- src/mezages/sack.py
+++ src/mezages/sack.py
@@ -1,7 +1,12 @@
-from typing import Any
-from mezages.subjects import get_subject_substitute
-from mezages.states import ensure_state, FormattedState
-from mezages.buckets import FormattedBucket, format_bucket
+from typing import Any, Optional
+from mezages.path import Path
+from mezages.subject import Subject
+from mezages.store import Store, OutputStore
+from mezages.bucket import Bucket, OutputMessages
+
+
+class SackError(Exception):
+    pass
 
 
 class Sack:
@@ -20,4 +25,24 @@ class Sack:
             subject_substitute = get_subject_substitute(path, self.__state)
             formatted_state[str(path)] = format_bucket(bucket, subject_substitute)
 
-        return formatted_state
+        return output_store
+
+    def union(self, store: dict[str, set[str]], mount_path: Optional[str] = None) -> None:
+        # validate arguments
+        Store.ensure(store)
+        if mount_path is not None:
+            Path.ensure(mount_path)
+
+        # if the mount_path exist, arrange the path accordingly
+        adjusted_store = {
+            Path(f'{mount_path}.{path}') if mount_path else Path(path): Bucket(bucket)
+            for path, bucket in store.items()
+        }
+
+        for path, bucket in adjusted_store.items():
+            if path in self.__store:
+                # unify the bucket if path already exist
+                self.__store[path].union(bucket)
+            else:
+                # copy path and bucket into the instance
+                self.__store[path] = bucket
