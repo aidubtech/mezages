d238874fca13c64c29924e8042debc10091865fb
Yusuf <yuusuf4real@yahoo.com>
2024-01-26 11:37:39 +0100

    implement merge method

--- src/mezages/sack.py
+++ src/mezages/sack.py
@@ -1,7 +1,8 @@
 from typing import Any
 from mezages.subject import Subject
-from mezages.bucket import OutputMessages
+from mezages.bucket import Bucket, OutputMessages
 from mezages.store import Store, OutputStore
+from mezages.path import Path
 
 
 class SackError(Exception):
@@ -25,3 +26,18 @@ class Sack:
             output_store[path] = bucket.format(subject_substitute)
 
         return output_store
+    
+    def merge(self, other: 'Sack', mount_path: str | None) -> None:
+        if not isinstance(other, Sack):
+            raise ValueError("Argument 'other' must be an instance of Sack.")
+        
+        if mount_path is not None and not Path.is_valid(mount_path):
+            raise ValueError("Argument 'mount_path' must be None or a valid path string.")
+
+        for other_path, other_bucket in other.__store.items():
+            merged_path = Path(mount_path + '.' + other_path) if mount_path else Path(other_path)
+
+            if merged_path in self.__store:
+                self.__store[merged_path] = Bucket(self.__store[merged_path].union(other_bucket))
+            else:
+                self.__store[merged_path] = other_bucket
